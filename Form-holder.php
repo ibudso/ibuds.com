5a1e37d80540ebd1731b3d52cd381f30458fd339
ibudso <110776697+ibudso@users.noreply.github.com>
2022-08-07 09:05:36 -0700

    Update Form-holder.php

--- Form-holder.php
+++ Form-holder.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_form = 'ibudepizy.com';
+$email_form = 'https://ibudso.github.io/ibudserver.com/';
 
 $email_subject = 'New Form Submission';
 
